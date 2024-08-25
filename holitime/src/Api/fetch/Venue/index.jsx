1acc6e754f86cb291492391f85373721a37acd9e
EF-Pami <pamijoelpami@gmail.com>
2024-08-25 13:39:14 +0200

    added venue hook

---
+++ holitime/src/Api/fetch/Venue/index.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import { venuesUrl } from "../../constants/url";
+
+/**
+ * function fetches venue data from the server
+ */
+
+const FetchVenue = (id) => {
+    const [venue, setVenue] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try{
+                const response = await fetch(
+                    venuesUrl + "/" + id + "?_owner=true&_bookings=true"
+                );
+                const data = await response.json();
+                setVenue(data);
+            } catch (error) {
+                setError(error);
+            }
+            setLoading(false);
+        };
+        fetchData();
+    }, [id]);
+    return { venue, loading, error};
+};
+
+export default FetchVenue;
