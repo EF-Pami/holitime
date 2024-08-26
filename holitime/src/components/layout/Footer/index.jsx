0281a63d5a8fb3c29ce3e6c666502deaecb85b5c
EF-Pami <pamijoelpami@gmail.com>
2024-08-26 22:12:08 +0200

    added layout folder

---
+++ holitime/src/components/layout/Footer/index.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import { Twitter } from "@mui/icons-material";
+
+export default function Footer() {
+    return (
+        <StyledFooter>
+            <div className="socialsContainer">
+                <FacebookIcon fontSize="large" sx={{color: "blue"}} />
+                <InstagramIcon fontSize="large" sx={{color: "blue"}} />
+            </div>
+            <p>&copy; HoliTime</p>
+        </StyledFooter>
+    );
+}
