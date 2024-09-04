ed6914d94eb49946d420a07dc65983bd7d6167f7
EF-Pami <pamijoelpami@gmail.com>
2024-09-04 20:18:27 +0200

    added home page

---
+++ holitime/src/pages/Home/index.jsx
@@ -0,0 +1,20 @@
+import React, { useContext} from "react";
+import Cards from "../../components/Cards";
+import ThemeContext from "../../components/Theme";
+
+/**
+ * function displays the home component
+ */
+
+const Home = () => {
+    const { theme} = useContext(ThemeContext);
+
+    return (
+        <div className={`${theme === "light" ? "bg-background text-secondary" : "bg-secondary text-primary"}`}>
+            <h1 className="text-xl text-center">Featured Venues</h1>
+            <Cards/>
+        </div>
+    );
+};
+
+export default Home;
